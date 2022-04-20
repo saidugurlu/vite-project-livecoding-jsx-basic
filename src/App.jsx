f3173e8fa52c41de37bc80316ef33bef0fe6f067
saidugurlu <ugurlumsaid@gmx.de>
2022-04-20 10:19:22 +0200

    colors added with array and with map showed

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,26 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from "react";
+
+import "./App.scss";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const title = "Info Site";
+  const colors = ["yellow", "green", "orange", "blue"];
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <h1>{title}</h1>
+      {colors.length > 0 && (
+        <>
+          <h2>Colors</h2>
+          <ul>
+            {colors.map((color, index) => {
+              return <li key={index}>{color}</li>;
+            })}
+          </ul>
+        </>
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
